a91d9130158d9a832ce43daf49883fd0dc8971c4
Vadim58 <awesome.rdeg@yandex>
2021-07-20 20:45:26 +0300

    Commit again

--- public/js/api/Entity.js
+++ public/js/api/Entity.js
@@ -1,16 +1,7 @@
-/**
- * Класс Entity - базовый для взаимодействия с сервером.
- * Имеет свойство URL, равно пустой строке.
- * */
+
 class Entity {
   URL = '';
   
-
-  /*
-   * Запрашивает с сервера список данных.
-   * Это могут быть счета или доходы/расходы
-   * (в зависимости от того, что наследуется от Entity)
-   * */
   static list(data, callback){
   const xhr = new XMLHttpRequest();
   xhr.open('GET', this.URL);
@@ -18,19 +9,17 @@ class Entity {
   createRequest(data, callback);
   }  
 
-  /**
-   * Создаёт счёт или доход/расход с помощью запроса
-   * на сервер. (в зависимости от того,
-   * что наследуется от Entity)
-   * */
   static create(data, callback) {
-
+    const xhr = new XMLHttpRequest();
+    xhr.open('PUT', this.URL);
+    xhr.send();
+    createRequest(data, callback);
   }
 
-  /**
-   * Удаляет информацию о счёте или доходе/расходе
-   * (в зависимости от того, что наследуется от Entity)
-   * */
   static remove(data, callback ) {
+    const xhr = new XMLHttpRequest();
+    xhr.open('DELETE', this.URL);
+    xhr.send();
+    createRequest(data, callback);
   }
 }
