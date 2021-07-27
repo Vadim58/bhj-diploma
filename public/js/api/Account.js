72d9cda4a124883a92667b759e10a9b789429c48
Vadim58 <awesome.rdeg@yandex>
2021-07-27 17:42:56 +0300

    new commit

--- public/js/api/Account.js
+++ public/js/api/Account.js
@@ -1,13 +1,11 @@
-/**
- * Класс Account наследуется от Entity.
- * Управляет счетами пользователя.
- * Имеет свойство URL со значением '/account'
- * */
-class Account extends Entity {
-  /**
-   * Получает информацию о счёте
-   * */
-  static get(id = '', callback){
 
+class Account extends Entity {
+  static URL = '/account';
+  static get(id, callback){
+    return createRequest({
+      url: `${this.URL}/${id}`,
+      method: 'GET',
+      callback
+    })
   }
 }
